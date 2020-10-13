b17052a0dfc9cc4ce8340f6d7fb22de8c357c498
Rajeev <68476724+rajeev7353@users.noreply.github.com>
2020-10-13 18:25:49 +0100

    Update Hello.java
    added some text from GitHub for practice

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("really awesine hello");
+        System.out.println("really  ADDED TEXT FROM GITHU awesome hello");
 
 
     }
