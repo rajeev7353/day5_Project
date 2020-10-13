239c1989513fd5a6279140c72c554674137a677c
Rajeev Dahal <rajeev88@gmail.com>
2020-10-13 17:47:29 +0100

    more line has been added

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -2,5 +2,8 @@ public class Feature2 {
 
     public static void main(String[] args) {
         System.out.println("Working on feature 2 to see the changes ");
+
+        System.out.println("Working  more on feature 2");
+        System.out.println("Working more and more on feature 2");
     }
 }
